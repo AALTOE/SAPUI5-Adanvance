73951af866925d9dc8bf58e0783d8d3ae2a087b2
AALTOE <alex.alto.espiri@gmail.com>
2024-11-08 06:13:28 +0000

    2- Elementos y Controladores UI5

--- webapp/controller/MainView.controller.js
+++ webapp/controller/MainView.controller.js
@@ -1,12 +1,38 @@
 sap.ui.define([
     "sap/ui/core/mvc/Controller"
 ],
-function (Controller) {
-    "use strict";
+    /**
+     * 
+     * @param {sap.ui.core.mvc.Controller} Controller 
+     * @returns 
+     */
+    function (Controller) {
+        "use strict";
 
-    return Controller.extend("logaligroup.logali.controller.MainView", {
-        onInit: function () {
+        return Controller.extend("logaligroup.logali.controller.MainView", {
+            onInit: function () {
 
+            },
+
+        /**
+         * Función que valida la longitud dentro del input,
+         * mostrando una lista cuando la longitud es igual a 6 ó
+         * ocultando una lista cuando la longitud es menor a 6
+         */
+        onValidate: function () {
+            var inputEmployee = this.byId("inputEmployee");
+            var valueEmployee = inputEmployee.getValue();
+
+            if (valueEmployee.length === 6) {
+                //inputEmployee.setDescription("OK");
+                this.byId("labelCountry").setVisible(true);
+                this.byId("slCountry").setVisible(true);
+            } else {
+                //inputEmployee.setDescription("NOT OK");
+                this.byId("labelCountry").setVisible(false);
+                this.byId("slCountry").setVisible(false);
+            }
         }
+
+        });
     });
-});
